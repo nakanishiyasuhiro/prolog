53eda4dd121a9c1bb2cc76e547d0ec150920df83
nakanishiyasuhiro <60539407+nakanishiyasuhiro@users.noreply.github.com>
2020-03-20 20:39:49 +0900

    view_fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: "tops#index"
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks',
   } 
   resources :programminglogs do
     resources :likes, only: [:create, :destroy]
